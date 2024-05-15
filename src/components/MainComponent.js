f3e26381ed545b71485511a2a60054a6d331b960
Antara Kundu <61645954+antarakundu08@users.noreply.github.com>
2024-05-15 15:31:43 +0530

    shimmer fixes

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import Shimmer from './Shimmer'
 import { VideoBackground } from './VideoBackground'
 import VideoTitle from './VideoTitle'
 
 const MainComponent = () => {
     
   const movie = useSelector((store) => store.movie?.nowPlayingMovies)
-  if (!movie) return <Shimmer />
+  if (!movie) return 
   const {id} = movie[0]
   return (
     <div className="pt-[30%] bg-black md:pt-0">
